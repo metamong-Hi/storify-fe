893974bd3b32b84fbba1ec6406b181665d38d2b1
이서진 <seojinlee@iseojin.local>
2024-01-17 15:18:05 +0900

    add header and reading books page with new app routing

---
+++ src/app/book/[bookId]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyBook from "../../components/MyBook";
+import Header from "../../components/Header"
+export default function Page({ params }: { params: { bookId: string } }) {
+  const bookId = params.bookId; // Assign the bookId to a variable
+  return (
+    <div className="flex flex-col h-screen">
+        <Header/>
+       <MyBook bookId={params.bookId} />
+    </div>
+  );
+}
