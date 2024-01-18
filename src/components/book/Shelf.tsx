941887a2e30bb85693e803acbb034cc3a8d67d35
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-18 19:42:27 +0900

    2024.01.18  책장 슬라이드 형식 초안

--- src/components/book/Shelf.tsx
+++ src/components/book/Shelf.tsx
@@ -1,44 +1,41 @@
-'use client';
-
-import Link from 'next/link';
 import React, { useEffect, useState, useRef, useCallback, RefObject } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import 'tailwindcss/tailwind.css';
 
-// import "./Bookshelf.module.css";
-// Assuming the imported image is processed and placed in the public folder
-
-import Image from 'next/image';
-
 interface BookshelfProps {
+    key: number;
     books: { title: string }[];
 }
 
-const Shelf: React.FC<BookshelfProps> = ({ books }) => {
+const Shelf: React.FC<BookshelfProps> = ({ key, books = [] }) => {
+    const rows = [books.slice(0, 3), books.slice(3, 6)];
     return (
-        <div className="relative">
-            <Image
-                src="/textures/bookshelf/bookShelf.png" // Correct the path
-                alt="Bookshelf"
-                width={500} // Example width
-                height={500} // Example height
-                className="w-full h-full" // Adjust these classes as needed
-                layout="responsive" // Optional: This will maintain aspect ratio
-            />
-            <div className="absolute top-0 left-0">
-                {books.map((book, index) => (
-                    <div
-                        key={index}
-                        className="w-24 h-32 shadow-lg rounded flex justify-center items-center text-xs font-medium text-gray-700 bg-white"
-                    >
-                        <Link href={`/book/${encodeURIComponent(book.title)}`}>
-                            <div onClick={() => console.log(`Book ${book.title} clicked`)}>
-                                {book.title}
+        <div
+            key={key}
+            className="flex justify-start"
+            style={{
+                backgroundImage: 'url(/textures/bookshelf/bookShelf.png)',
+                width: 'full',
+                height: 'full',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            {rows.map((row, rowIndex) => (
+                <div key={rowIndex} className="book-row">
+                    {row.map((book, bookIndex) => (
+                        <Link key={bookIndex} href={`/book/${encodeURIComponent(book.title)}`}>
+                            <div className="relative w-12 h-16 shadow-lg rounded flex justify-center items-center text-xs font-medium text-gray-700 bg-white m-2">
+                                <p className="flex justify-center items-center text-center">
+                                    {book.title}
+                                </p>
                             </div>
                         </Link>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 };
