c3749b322308124d467b2be29c92cb1969243a11
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-19 21:03:10 +0900

    feat : 책장 수정

--- src/components/book/Shelf.tsx
+++ src/components/book/Shelf.tsx
@@ -1,50 +1,52 @@
-// BookShelves.tsx
-'use client';
+import React, { useEffect, useState, useRef, useCallback, RefObject } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import React, { ReactElement, useEffect, useState } from 'react';
-import Shelf from './Shelf';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; // Swiper styles
-import { getAllBooks } from './AllBooks';
+import 'tailwindcss/tailwind.css';
+import { BooksData } from '@/types/books';
 
-export default function BookShelves() {
-    const [bookShelves, setBookShelves] = useState<ReactElement[]>([]);
-    async function settingBookShelves() {
-        const allbooks = await getAllBooks();
-        console.log(allbooks);
-        const bookShelves = () => {
-            return (
-                <>
-                    {allbooks.map((shelf, index) => (
-                        <SwiperSlide key={index}>
-                            <Shelf key={index} books={shelf} />
-                        </SwiperSlide>
-                    ))}
-                </>
-            );
-        };
+interface BookshelfProps {
+    key: number;
+    books: BooksData[];
+}
 
-        setBookShelves(() => [bookShelves()]);
-    }
-    useEffect(() => {
-        settingBookShelves();
-    }, []);
-
+const Shelf: React.FC<BookshelfProps> = ({ key, books = [] }) => {
+    const rows = [books.slice(0, 2), books.slice(2, 4), books.slice(4, 6)];
     return (
-        <div className="flex justify-center items-center ">
-            <Swiper
-                spaceBetween={0}
-                slidesPerView={2}
-                centeredSlides={true}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                style={{
-                    height: '100vh',
-                    width: '100vw',
-                }}
-            >
-                {bookShelves ? bookShelves : <></>}
-            </Swiper>
+        <div
+            className="flex flex-col justify-center items-center bg-no-repeat bg-center"
+            style={{
+                backgroundImage: 'url(/textures/bookshelf/bookShelf.png)',
+                backgroundSize: 'auto',
+                height: '100vh',
+                width: '100vw',
+            }}
+        >
+            {rows.map((row, rowIndex) => (
+                <div key={rowIndex} className="flex justify-center items-center ">
+                    {row.map((book, bookIndex) => (
+                        <Link
+                            key={bookIndex}
+                            href={`/book/${encodeURIComponent(book._id)}`}
+                            className="flex justify-center items-center"
+                        >
+                            <div
+                                className="relative shadow-lg rounded flex justify-center items-center text-xs font-medium text-gray-700 bg-white ml-5 mb-10"
+                                style={{
+                                    width: '7vw',
+                                    height: '14vh',
+                                }}
+                            >
+                                <p className="flex text-center justify-center items-center ">
+                                    {book.title}
+                                </p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            ))}
         </div>
     );
-}
+};
+
+export default Shelf;
