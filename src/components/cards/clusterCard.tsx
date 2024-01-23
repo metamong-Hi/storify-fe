3b51ca0e889854bcfd03a25d686d52102b246725
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-23 16:21:09 +0900

    fix : 카드 수정

--- src/components/cards/clusterCard.tsx
+++ src/components/cards/clusterCard.tsx
@@ -3,19 +3,30 @@ import './clusterCard.css';
 
 interface ClusterCardProps {
     className?: string;
+    size?: string;
     onClick: () => void;
-  }
-const ClusterCard: React.FC<ClusterCardProps> = ({ className, onClick }) => {
-  const [isRotated, setIsRotated] = useState(false);
-
-  return (
-    <div
-    className={`card ${className ? className : ''}`}
-    onClick={onClick}
-    >
-      1
-    </div>
-  );
+}
+const ClusterCard: React.FC<ClusterCardProps & { onFlyOffEnd: () => void }> = ({
+    className,
+    size,
+    onClick,
+    onFlyOffEnd,
+}) => {
+    const [isRotated, setIsRotated] = useState(false);
+    const handleAnimationEnd = (event: { animationName: string }) => {
+        if (event.animationName === 'flyOff') {
+            onFlyOffEnd();
+        }
+    };
+    return (
+        <div
+            className={`card ${className ? className : ''}`}
+            onClick={onClick}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            1
+        </div>
+    );
 };
 
 export default ClusterCard;
