f68f19348a48305d71def02a67634f2420b97caf
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-23 15:27:06 +0900

    수정 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,7 @@ import Header from '@/components/Header';
 import BookShelves from '@/components/book/BookShelves';
 export default function Home() {
     return (
-        <main
-            className="flex justify-center items-center bg-no-repeat"
-            style={{
-                backgroundImage: 'url(images/backGround/brickBackground.jpg)',
-                backgroundSize: 'fill',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'bottom',
-            }}
-        >
+        <main className="flex justify-center items-center">
             <div className="flex justify-center items-center">
                 <BookShelves />
             </div>
