3d08e407a8fdeeddf731f6628cf8f8dc8f93fca8
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-25 12:00:41 +0900

    design : 책장 카드 스타일 변경

--- src/components/book/BookShelves.tsx
+++ src/components/book/BookShelves.tsx
@@ -4,11 +4,14 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import Book from './Book';
 
 import { getAllBooks } from './AllBooks';
-import Pagination from './Pagination'; // Pagination 컴포넌트를 추가합니다.
+// import Pagination from './Pagination'; // Pagination 컴포넌트를 추가합니다.
+import { Pagination, Spinner } from '@nextui-org/react';
+import BookSkeleton from '../skeleton/BookSkeleton';
 
 export default function BookShelves() {
     const [bookShelves, setBookShelves] = useState<ReactElement[]>([]);
     const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 추가
+    const variants = ['flat', 'bordered', 'faded', 'light'];
 
     async function settingBookShelves(page: number) {
         const allbooks = await getAllBooks(page);
@@ -23,16 +26,11 @@ export default function BookShelves() {
     }, [currentPage]);
 
     return (
-        <div
-            className="bg-gray-100 p-2"
-            style={{
-                width: '100vw',
-            }}
-        >
+        <div className="bg-gray-100 p-2">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
-                {bookShelves.length > 0 ? bookShelves : <p>Loading...</p>}
+                {bookShelves.length > 0 ? bookShelves : <BookSkeleton />}
             </div>
-            <Pagination currentPage={currentPage} setPage={setCurrentPage} />
+            <Pagination key={'light'} total={10} initialPage={1} variant={'light'} />
         </div>
     );
 }
---
+++ src/components/skeleton/BookSkeleton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Card, CardFooter, Image, Skeleton } from '@nextui-org/react';
+
+const BookSkeleton = () => {
+    return new Array(10).fill('').map((_, i) => (
+        <Card isFooterBlurred radius="lg" className="border-none" key={i}>
+            <Skeleton className="rounded-lg">
+                <Image alt="Book Cover Image" className="object-cover" height={1000} width={1000} />
+            </Skeleton>
+            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <div className="flex justify-between items-center">
+                    <Skeleton className="w-3/5 rounded-lg">
+                        <div className="text-gray-600 text-sm"></div>
+                    </Skeleton>
+                </div>
+                <div className="flex justify-between items-center">
+                    <Skeleton className="w-3/5 rounded-lg">
+                        <div className="text-gray-600 text-sm"></div>
+                    </Skeleton>
+                    <Skeleton className="w-3/5 rounded-lg">
+                        <div className="text-gray-600 text-sm"></div>
+                    </Skeleton>
+                </div>
+            </CardFooter>
+        </Card>
+    ));
+};
+
+export default BookSkeleton;
