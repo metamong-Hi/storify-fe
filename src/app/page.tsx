0ad4dc7223434d8a409c790f9cd2a2713e0e2e1a
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-26 00:12:04 +0900

    feat : 홈 화면 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Link from 'next/link';
 export default function Home() {
-  return (
-    <main className="flex justify-center items-center">
-      <div className="flex justify-center items-center">
-        <Link href="/allbooks">AllBooks</Link>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex justify-center items-center">
+            <div className="flex justify-center items-center">MAIN</div>
+        </main>
+    );
 }
