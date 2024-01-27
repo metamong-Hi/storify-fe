74bbbb6539bddf2569a2a7dfe795b1e5a119e5ec
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-28 04:28:50 +0900

    build : 파일 수정

--- src/app/allbooks/page.tsx
+++ src/app/allbooks/page.tsx
@@ -4,65 +4,8 @@ import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import BookShelves from '@/components/book/BookShelves';
 import { getAllBooks } from '@/components/book/AllBooks';
 import { BooksData } from '@/types/books';
-import { Pagination, PaginationItemType, PaginationItemRenderProps } from '@nextui-org/react';
+import { Pagination } from '@nextui-org/react';
 import PaginationSkeleton from '@/components/skeleton/PaginationSkeleton';
-import { ChevronIcon } from '@/components/icons/ChevronIcon';
-import cn from 'classnames';
-
-const renderItem = ({
-  ref,
-  key,
-  value,
-  isActive,
-  onNext,
-  onPrevious,
-  setPage,
-  className,
-}: PaginationItemRenderProps<HTMLButtonElement>) => {
-  if (value === PaginationItemType.NEXT) {
-    return (
-      <button
-        key={key}
-        className={cn(className, 'bg-[#B68973]/50 min-w-8 w-8 h-8')}
-        onClick={onNext}
-      >
-        <ChevronIcon className="rotate-180" />
-      </button>
-    );
-  }
-
-  if (value === PaginationItemType.PREV) {
-    return (
-      <button
-        key={key}
-        className={cn(className, 'bg-[#B68973]/50 min-w-8 w-8 h-8')}
-        onClick={onPrevious}
-      >
-        <ChevronIcon />
-      </button>
-    );
-  }
-
-  if (value === PaginationItemType.DOTS) {
-    return (
-      <button key={key} className={className}>
-        ...
-      </button>
-    );
-  }
-
-  // cursor is the default item
-  return (
-    <button
-      ref={ref}
-      key={key}
-      className={cn(className, isActive && 'text-white bg-[#B68973] font-bold')}
-      onClick={() => setPage(value)}
-    >
-      {value}
-    </button>
-  );
-};
 
 const Page = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -91,20 +34,22 @@ const Page = () => {
       <div className="flex flex-col ">
         <BookShelves books={bookShelves} limit={limit} />
         {totalPages ? (
-          <Pagination
-            disableCursorAnimation
-            showControls
-            total={totalPages}
-            page={currentPage}
-            initialPage={1}
-            className="gap-2 flex justify-center items-center"
-            radius="full"
-            renderItem={renderItem}
-            variant="light"
-            onChange={(page: number) => {
-              setCurrentPage(page);
-            }}
-          />
+          <span className="flex justify-center items-center">
+            <Pagination
+              classNames={{
+                item: 'w-8 h-8 text-small rounded-none bg-transparent',
+                cursor:
+                  'bg-gradient-to-b shadow-lg from-[#B68973] to-default-800 dark:from-[#B68973] dark:to-default-100 text-white font-bold',
+              }}
+              key={'flat'}
+              total={totalPages}
+              initialPage={1}
+              page={currentPage}
+              onChange={(page: number) => {
+                setCurrentPage(page);
+              }}
+            />
+          </span>
         ) : (
           <PaginationSkeleton />
         )}
