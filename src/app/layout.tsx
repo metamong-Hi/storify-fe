b6fa59dada3ce21851498ac3feaa0278455d1d1b
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-28 01:06:10 +0900

    design : 어린아이 글꼴 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,32 @@
 'use client';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+
 import './globals.css';
-import Header from '@/components/Header';
+
 import { Provider } from 'react-redux';
 import { store } from '../store/index';
-const inter = Inter({ subsets: ['latin'] });
+
 import { Providers } from './providers';
 import NavbarComponent from '@/components/NavbarComponent';
 
+import localFont from 'next/font/local';
+
+const myFont = localFont({
+  src: [
+    { path: 'fonts/210KidsR.woff2', weight: '500', style: 'normal' },
+    { path: 'fonts/210KidsB.woff2', weight: '700', style: 'thick' },
+    { path: 'fonts/210KidsL.woff2', weight: '300', style: 'thin' },
+  ],
+  display: 'swap',
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="kr" className={myFont.className}>
       <body>
         <Provider store={store}>
           <Providers>
             <NavbarComponent />
-
-              {children}
-            {/* </div> */}
+            {children}
           </Providers>
         </Provider>
       </body>
