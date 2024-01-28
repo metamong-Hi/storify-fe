4e61757b2b7de6008b90c79ef2c4c5ed8a0154e7
metamong-Hi <seojinlee27@naver.com>
2024-01-28 17:44:23 +0900

    comflict

--- src/components/objects/cards/simple.tsx
+++ src/components/objects/cards/simple.tsx
@@ -2,38 +2,40 @@
 import React from "react";
 import { Card, CardFooter, Image, Button } from "@nextui-org/react";
 import Link from 'next/link';
+
 const SimpleCard = () => {
   const handleCardClick = () => {
     window.location.href = '/writing/simpleWriting';
   };
+
   return (
     <Card
       isFooterBlurred
       isPressable
       radius="lg"
-      className="border-[none] w-1/3 h-2/3 sm: mr-0 md:mr-6 lg:mr-6 mt-10"
+      className="border-[none] w-1/3 h-2/3 sm: mr-0 md:mr-6 lg:mr-6"
       onClick={handleCardClick} // Card에 onClick 이벤트 추가
     >
       <Image
         isZoomed
         alt="동화 생성 선택"
         className="object-cover"
-<<<<<<< HEAD
         src="/images/pictures/sample1.png"
-=======
-        src= "https://storify-fe.vercel.app/images/pictures/sample1.png"
->>>>>>> 18ead74615d6fee0445ded2a9a2142458b6cd702
-        width="100%"  
-        height="100%"
+        width="100"  
+        height="100"
       />
 
       <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-      <span 
-          className="text-black text-xl" 
-          style={{ fontSize: "1.5rem" }}
+        <Button
+          className="text-tiny text-black"
+          variant="light"
+          color="primary"
+          radius="lg"
+          size="lg"
+          style={{ fontSize: '1.25rem' }}
         >
           간단히 만들기
-          </span>
+        </Button>
       </CardFooter>
     </Card>
   );
