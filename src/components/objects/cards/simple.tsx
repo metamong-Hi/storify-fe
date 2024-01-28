02b67611678692b3cfc9ea4f2d324a38d3e9e86a
metamong-Hi <seojinlee27@naver.com>
2024-01-28 16:26:46 +0900

    헤더 색상 바꿈

--- src/components/objects/cards/simple.tsx
+++ src/components/objects/cards/simple.tsx
@@ -21,8 +21,8 @@ const SimpleCard = () => {
         alt="동화 생성 선택"
         className="object-cover"
         src="/images/pictures/sample1.png"
-        width="100%"  
-        height="100%"
+        width={100}  
+        height={100}
       />
 
       <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
