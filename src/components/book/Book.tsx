7fd497aa01045e719159b1c301bdf61c3e631a16
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-29 23:15:49 +0900

    design : NextUI -> daisyUI

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,74 +1,81 @@
 import React from 'react';
 import Link from 'next/link';
-import { BooksData } from '@/types/books';
-import { Card, CardFooter, CardBody, CardHeader, Image, Button, Tooltip } from '@nextui-org/react';
+import Image from 'next/image';
 import { HeartIcon } from '@/components/icons/HeartIcon';
 
+import { BooksData } from '@/types/books';
+
 interface BookProps {
-  Book: BooksData;
+  book: BooksData;
   index: number;
 }
 
-const Book = ({ Book, index }: BookProps) => {
+const Book = ({ book, index }: BookProps) => {
   const [liked, setLiked] = React.useState(false);
   let imageURL;
 
   try {
-    const body = Book.body || null;
+    const body = book.body || null;
     const noBookImg = body ? body[1].imageUrl : '/images/pictures/noBookImg.png';
-    imageURL = Book.coverUrl || noBookImg;
+    imageURL = book.coverUrl || noBookImg;
   } catch (error) {
     imageURL = '/images/pictures/noBookImg.png';
   }
 
-  let token = '' as string;
+  let token = '';
   if (typeof window !== 'undefined') {
     token = localStorage.getItem('token') ?? '';
   }
 
+  const user = {
+    avatar: '/images/pictures/noBookImg.png', // Replace with actual path to user's avatar
+    bookshelfLink: `/user/${encodeURIComponent(book.userId?._id ?? '')}/bookshelf`, // Replace with actual link to user's bookshelf
+    name: book.userId?.username ?? '', // Replace with actual user's name
+  };
+
   return (
-    <Card isFooterBlurred key={index} radius="lg" className="border-none relative">
-      <Link href={`/book/${encodeURIComponent(Book._id)}`}>
-        <Image
-          isZoomed
-          src={imageURL}
-          alt="Book Cover Image"
-          className="object-cover"
-          height={500}
-          width={500}
-          loading="lazy"
-        />
-      </Link>
-
-      {/* <CardHeader className="p-0">
-        <div className="absolute z-10 top-2 left-2 text-default-900/60 bg-white/60 border-black border-1 hover:bg-white">
-          <div className="text-black-600 text-sm md:text-lg lg:text-xl">{Book.count} views</div>
-          <div className="text-black-600 text-sm md:text-lg lg:text-xl">{Book.rate} likes</div>
-        </div>
-        {token ? (
-          <Button
-            isIconOnly
-            className="absolute z-10 top-2 right-2 text-default-900/60 bg-white/60 border-black border-1 hover:bg-white"
-            onPress={() => setLiked((v) => !v)}
-          >
+    <div
+      key={index}
+      className="relative bg-white bg-opacity-10 backdrop-blur-sm rounded-lg overflow-hidden shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-2xl"
+    >
+      <figure className="w-full">
+        <Link href={`/book/${encodeURIComponent(book?._id ?? '')}`}>
+          <Image
+            src={imageURL ?? ''}
+            alt="Book Cover Image"
+            layout="responsive"
+            height={160} // Adjust the height to match your design
+            width={280} // Adjust the width to match your design
+            className="transition-transform duration-500 hover:scale-105"
+          />
+        </Link>
+      </figure>
+      <div className="p-4">
+        <div className="truncate w-full text-lg md:text-xl lg:text-2xl font-bold">{book.title}</div>
+        <div className="flex justify-between items-center mt-2">
+          <Link href={user.bookshelfLink}>
+            <div className="flex items-center space-x-2">
+              <div className="avatar">
+                <div className="w-8 h-8 rounded-full">
+                  <Image src={user.avatar} alt={`${user.name}'s Avatar`} layout="fill" />
+                </div>
+              </div>
+              <span className="text-sm font-semibold">{user.name}</span>
+            </div>
+          </Link>
+          <button className="btn btn-ghost btn-circle" onClick={() => setLiked(!liked)}>
             <HeartIcon
-              className={liked ? '[&>path]:stroke-transparent' : ''}
-              fill={liked ? '#fc3c3c' : 'none'}
+              className={`w-6 h-6 ${liked ? 'fill-current text-red-500' : 'text-gray-500'}`}
             />
-          </Button>
-        ) : null}
-      </CardHeader> */}
-
-      <CardFooter className="text-[#1E212D] justify-center font-bold bg-white/50 border-white/10 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] h-[calc(15%_-_8px)] shadow-small ml-1 z-10">
-        <div className=" whitespace-nowrap overflow-hidden text-overflow-ellipsis text-black-600 text-sm md:text-base lg:text-lg">
-          {Book.title}
+          </button>
         </div>
-        {/* <div className="flex flex-col justify-between items-center">
-          <div className="text-black-600 text-sm md:text-lg lg:text-xl">{Book.count} views</div>
-          <div className="text-black-600 text-sm md:text-lg lg:text-xl">{Book.rate} likes</div>
-        </div> */}
-      </CardFooter>
-    </Card>
+
+        <div className="flex justify-between items-center mt-4">
+          <span className="text-sm">{book.rate} views</span>
+          <span className="text-sm">{book.count} likes</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/book/BookShelves.tsx
+++ src/components/book/BookShelves.tsx
@@ -15,7 +15,7 @@ export default function BookShelves({ books, limit, search }: BookShelvesProps) 
       {books.length > 0 ? (
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5">
           {books.map((book, index) => (
-            <Book key={index} Book={book} index={index} />
+            <Book key={index} book={book} index={index} />
           ))}
         </div>
       ) : search ? (
