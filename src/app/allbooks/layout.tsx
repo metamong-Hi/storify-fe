16cd62beb651b216afd0c2938049fa8d6b9930d1
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-02-01 13:03:24 +0900

    design :  배경 해결

--- src/app/allbooks/layout.tsx
+++ src/app/allbooks/layout.tsx
@@ -1,5 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex justify-center items-center h-[92vh] bg-[#FAF3E0]/80">{children}</div>
-  );
+  return <div className="flex justify-center items-center bg-[#FAF3E0]/80">{children}</div>;
 }
