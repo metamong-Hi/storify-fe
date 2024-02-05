ce9cb508f13ebe59499bd8aae3c39dbe51c55cbf
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-02-05 10:53:51 +0900

    design : 글꼴 적용

--- src/app/allbooks/layout.tsx
+++ src/app/allbooks/layout.tsx
@@ -1,3 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="flex justify-center items-center bg-[#FAF3E0]/80">{children}</div>;
+  return (
+    <div className="flex justify-center items-center bg-[#FAF3E0]/80 font-NamuGulim">
+      {children}
+    </div>
+  );
 }
