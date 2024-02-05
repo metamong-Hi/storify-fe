8a7d12f8dda74a7490fb85746167389d01a67431
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-02-05 15:51:02 +0900

    feat : 책장 컴퍼넌트 분리

--- src/app/allbooks/page.tsx
+++ src/app/allbooks/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { getAllBooks } from '@/components/book/AllBooks';
 import BooksPage from '@/components/book/BooksPage';
+import { BooksData } from '@/types/books';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 const Page = () => {
   return (
     <div className="container flex justify-center items-center p-5">
-      <BooksPage />
+      <BooksPage getBooks={getAllBooks} userId="" />
     </div>
   );
 };
--- src/hooks/useBooksData.ts
+++ src/hooks/useBooksData.ts
@@ -2,28 +2,50 @@
 import { useState, useEffect, useCallback } from 'react';
 import { getAllBooks } from '@/components/book/AllBooks';
 import { BooksData } from '@/types/books';
-
-const useBooksData = (currentPage: number, limit: number, sortBy: string, search: string) => {
+import { GetParams } from '@/components/book/AllBooks';
+interface UseBooksDataProps {
+  getBooksFunction: (
+    page: number,
+    limit: number,
+    sort: string,
+    search: string,
+    id: string,
+  ) => Promise<{ books: BooksData[]; total: number }>;
+  currentPage: number;
+  limit: number;
+  sortBy: string;
+  search: string;
+  userId: string;
+}
+const useBooksData = ({
+  getBooksFunction,
+  currentPage,
+  limit,
+  sortBy,
+  search,
+  userId,
+}: UseBooksDataProps) => {
   const [bookShelves, setBookShelves] = useState<BooksData[]>([]);
   const [totalItems, setTotalItems] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const data = await getAllBooks(currentPage, limit, sortBy, search);
-      setBookShelves(data.books);
-      setTotalItems(data.total);
-    } catch (error) {
-      console.error('Failed to fetch books:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [currentPage, limit, sortBy, search]);
-
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        console.log('fetchData', currentPage, limit, sortBy, search, userId);
+        const data = await getBooksFunction(currentPage, limit, sortBy, search, userId);
+        setBookShelves(data.books);
+        setTotalItems(data.total);
+      } catch (error) {
+        console.error('Failed to fetch books:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchData();
-  }, [fetchData]);
+  }, [currentPage, limit, sortBy, search, userId, getBooksFunction]);
 
   return { bookShelves, totalItems, isLoading };
 };
