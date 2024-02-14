4402112f93f84c96ce17a731e49c607cb02a32e8
김병현(40) <129952654+sirloinbh@users.noreply.github.com>
2024-02-14 17:12:49 +0900

    testbh 페이지 비우기

--- src/app/test-bh/page.tsx
+++ src/app/test-bh/page.tsx
@@ -5,36 +5,11 @@ import BackgroundMusic from '@/components/Setting/BackgroundMusic';
 import FontSelector from '@/components/Setting/Font';
 
 const TestBH: React.FC = () => {
-  const [currentTab, setCurrentTab] = useState('music');
 
-  const renderComponent = () => {
-    switch (currentTab) {
-      case 'music':
-        return <BackgroundMusic />;
-      case 'theme':
-        return <SettingsComponent />;
-      case 'font':
-        return <FontSelector />;
-      default:
-        return null; // Or a default component
-    }
-  };
 
   return (
-    <div className="min-h-screen min-w-[80vw] flex flex-col justify-center items-center text-center">
-      <div role="tablist" className="tabs tabs-lifted">
-        <a role="tab" className={`tab ${currentTab === 'theme' ? 'tab-active' : ''}`} onClick={() => setCurrentTab('theme')}>
-          테마
-        </a>
-        <a role="tab" className={`tab ${currentTab === 'font' ? 'tab-active' : ''}`} onClick={() => setCurrentTab('font')}>
-          글꼴
-        </a>
-        <a role="tab" className={`tab ${currentTab === 'music' ? 'tab-active' : ''}`} onClick={() => setCurrentTab('music')}>
-          배경음악
-        </a>
-      </div>
-      {renderComponent()}
-    </div>
+    <>
+    </>
   );
 };
 
