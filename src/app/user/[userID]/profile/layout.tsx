b09801597f0f86880e07f030bf5be20279735e63
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-02-22 21:46:01 +0900

    design : 색상 변경

--- src/app/user/[userID]/profile/layout.tsx
+++ src/app/user/[userID]/profile/layout.tsx
@@ -1,10 +1,10 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <div className="flex flex-col justify-center items-center mt-10">
-        <div className="text-base text-xl text-bold sm:text-2xl lg:text-3xl 2xl:text-4xl">
+      <div className="flex  flex-col justify-center items-center mt-10">
+        <span className="text-base-content text-xl text-bold sm:text-2xl lg:text-3xl 2xl:text-4xl">
           마이 페이지
-        </div>
+        </span>
         {children}
       </div>
     </section>
