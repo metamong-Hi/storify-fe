70658032c82b2e7ce7ea72324c77b42cb00179e4
metamong-Hi <seojinlee27@naver.com>
2024-02-23 17:39:44 +0900

    빌드 되어라 제발

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,13 @@
+import { ProfileData } from '@/types/user';
+import Swal from 'sweetalert2';
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const token = sessionStorage.getItem('token');
+
+const token = typeof window !== 'undefined' ? sessionStorage.getItem('token') : null;
 
 export async function updateUserPassword(oldPassword: string, newPassword: string): Promise<void> {
-  // console.log('token: ', token);
   try {
+    if (!token) throw new Error('No token found');
     await fetch(`${API_URL}/auth/password`, {
       method: 'PATCH',
       headers: {
@@ -13,7 +17,7 @@ export async function updateUserPassword(oldPassword: string, newPassword: strin
       body: JSON.stringify({ oldPassword, newPassword }),
     }).then(async (response) => {
       const data = await response.json();
-      // console.log(data);
+
       if (data.message === '비밀번호를 확인해주세요.') {
         alert('현재 비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
       } else {
@@ -26,9 +30,8 @@ export async function updateUserPassword(oldPassword: string, newPassword: strin
 }
 
 export async function updateUserEmail(email: string): Promise<void> {
-  const token = sessionStorage.getItem('token');
-
   try {
+    if (!token) throw new Error('No token found');
     await fetch(`${API_URL}/users`, {
       method: 'PATCH',
       headers: {
@@ -39,12 +42,16 @@ export async function updateUserEmail(email: string): Promise<void> {
         email,
       }),
     })
-      .then((response) => {
-        // console.log(response);
-        // console.log(response.json());
+      .then(async (response) => {
+        const data = await response.json();
+        if (data.message === '이메일을 확인해주세요.') {
+          alert('이메일 형식을 확인해주세요.');
+        } else {
+          alert('이메일 업데이트 성공');
+        }
       })
-      .then(() => {
-        // console.log('이메일 업데이트 성공');
+      .catch((error) => {
+        console.error('이메일 업데이트 실패: ', error);
       });
   } catch (error) {
     console.error('이메일 업데이트 실패: ', error);
@@ -57,7 +64,6 @@ export async function updateUserProfile(
   introduction: string,
 ): Promise<void> {
   try {
-    const token = sessionStorage.getItem('token');
     if (!token) throw new Error('No token found');
 
     const formData = new FormData();
@@ -66,6 +72,17 @@ export async function updateUserProfile(
       formData.set('avatar', avatar);
     }
     if (nickname) {
+      if (nickname.length > 10) {
+        throw Error('닉네임은 10자 이내로 입력해주세요.');
+      }
+      if (nickname.length < 2) {
+        throw Error('닉네임은 2자 이상 입력해주세요.');
+      }
+
+      if (nickname.match(/^[0-9a-zA-Zㄱ-ㅎ가-힣]*$/) == null) {
+        throw Error('닉네임은 한글, 영문, 숫자만 입력 가능합니다.');
+      }
+
       formData.set('nickname', nickname);
     }
     if (introduction) {
@@ -82,24 +99,27 @@ export async function updateUserProfile(
 
     if (!response.ok) {
       throw new Error('Failed to update profile');
+    } else {
+      Swal.fire({
+        icon: 'success',
+        title: '프로필 업데이트 성공',
+        text: '프로필이 업데이트 되었습니다.',
+      });
     }
-    const data = await response.json(); 
-
-    sessionStorage.setItem('nickname',data.nickname);
-    window.location.reload();
-  } catch (error) {
-    console.error('프로필 업데이트 실패: ', error);
+  } catch (error: any) {
+    throw new Error(error.message);
   }
 }
 
-
-export async function getUserInfo(_id: string): Promise<any> {
+export async function getUserInfo(_id: string): Promise<ProfileData | null> {
+  if (!_id ?? _id === '') {
+    return null;
+  }
   const response = await fetch(`${API_URL}/users/${_id}`, {
     method: 'GET',
   });
 
   if (!response.ok) {
-    // console.log('response: ', response);
     throw new Error('Failed to fetch user profile');
   }
 
