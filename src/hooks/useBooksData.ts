e3038b1e83f79ac9c53702b8c624a28b36ef4dba
metamong-Hi <seojinlee27@naver.com>
2024-02-23 17:22:13 +0900

    콘솔 로그 지우기

--- src/hooks/useBooksData.ts
+++ src/hooks/useBooksData.ts
@@ -1,39 +1,29 @@
-import { useState, useEffect } from 'react';
-import { getBooks } from '@/components/book/AllBooks';
+// useBooksData.ts
+import { useState, useEffect, useCallback } from 'react';
+import { getAllBooks } from '@/components/book/AllBooks';
 import { BooksData } from '@/types/books';
-
+import { GetParams } from '@/components/book/AllBooks';
 interface UseBooksDataProps {
-  type: string;
   currentPage: number;
   limit: number;
   sortBy: string;
   search: string;
   userId: string;
 }
-const useBooksData = ({
-  currentPage = 1,
-  limit = 24,
-  sortBy = '',
-  search = '',
-  userId = '',
-  type = '',
-}: UseBooksDataProps) => {
+const useBooksData = ({ currentPage, limit, sortBy, search, userId }: UseBooksDataProps) => {
   const [bookShelves, setBookShelves] = useState<BooksData[]>([]);
   const [totalItems, setTotalItems] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      // console.log('fetching data');
       setIsLoading(true);
       try {
-        const data = await getBooks(currentPage, limit, sortBy, search, userId, type);
-        if (type === 'liked') {
-          setBookShelves(data);
-          setTotalItems(data.length);
-        } else {
-          setBookShelves(data.books);
-          setTotalItems(data.total);
-        }
+        const data = await getAllBooks(currentPage, limit, sortBy, search, userId);
+        // console.log('data:', data);
+        setBookShelves(data.books);
+        setTotalItems(data.total);
       } catch (error) {
         console.error('Failed to fetch books:', error);
       } finally {
@@ -42,7 +32,7 @@ const useBooksData = ({
     };
 
     fetchData();
-  }, [currentPage, limit, sortBy, search, userId, type]);
+  }, [currentPage, limit, sortBy, search, userId]);
 
   return { bookShelves, totalItems, isLoading };
 };
